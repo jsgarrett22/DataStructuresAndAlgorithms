710f2980c7c88a5be060139060bd6c427f40bab3
Joshua Garrett <garrej01@email.franklin.edu>
2025-01-22 20:57:57 -0500

    Implementing adding to the head of the LinkedList

--- src/com/garrett/list/LinkedList.java
+++ src/com/garrett/list/LinkedList.java
@@ -2,13 +2,79 @@ package com.garrett.list;
 
 public class LinkedList {
 
+	private Node head;
 	private int size;
 	
-	public LinkedList() {
-		
-	}
+	// default constructor
+	public LinkedList() {}
 	
+	/**
+	 * Returns the number of elements in the list.
+	 * @return the number of elements of the list
+	 */
 	public int size() {
 		return size;
+	}
+	
+	/**
+	 * Adds an element to the end of the list. Always returns true.
+	 */
+	public boolean add(int value) {
+		Node node = new Node(value);
+		if (head == null) {
+			addFirst(node.data);
+		}
+		return true;
+	}
+	
+	/**
+	 * Adds an element to the head of the list.
+	 */
+	public void addFirst(int value) {
+		head = new Node(value, head);
+		size++;
+	}
+	
+	/**
+	 * Returns the value of the first element of the list.
+	 * @return the value of the first element of the list or -1 if the list is empty.
+	 */
+	public int getFirst() {
+		if (head != null) {
+			return head.data;
+		} else {
+			return -1;
+		}
+	}
+	
+	/**
+	 * Nested data structure that contains the value of the node and a reference
+	 * to the next node in the LinkedList.
+	 */
+	private class Node {
+		
+		int data;
+		Node next;
+		
+		/**
+		 * Creates a node given only a value and not a reference to a node next in the
+		 * list.
+		 * @param data the data value of the node
+		 */
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+		
+		/**
+		 * Creates a node given a particular value and an assigned reference to the next
+		 * node, otherwise it is null, signifying the end of the LinkedList.
+		 * @param data the data value of the node
+		 * @param next the next node in the list, otherwise it is null
+		 */
+		public Node(int data, Node next) {
+			this.data = data;
+			this.next = next;
+		}
 	}
 }
--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -9,9 +9,38 @@ import com.garrett.list.LinkedList;
 class LinkedListTest {
 
 	@Test
+	void defaultConstructorTest() {
+		LinkedList list = new LinkedList();
+		assertEquals(0, list.size());		// size should be given the default of zero
+		assertEquals(-1, list.getFirst());	// the head should be set to the default value of null
+	}
+	
+	@Test
+	void addTest() {
+		LinkedList list = new LinkedList();
+		// add first value of 5
+		assertTrue(list.add(5));
+		assertEquals(1, list.size());
+		assertEquals(5, list.getFirst());
+		// add second value of 7
+		assertTrue(list.add(7));
+		assertEquals(2, list.size());
+		assertEquals(5, list.getFirst());
+	}
+	
+	@Test
+	void addFirstTest() {
+		LinkedList list = new LinkedList();
+		// add first value of 5
+		list.addFirst(5);
+		assertEquals(1, list.size());
+		assertEquals(5, list.getFirst());
+	}
+	
+	@Test
 	void sizeTest() {
 		LinkedList list = new LinkedList();
 		assertEquals(0, list.size());
 	}
-
+	
 }
